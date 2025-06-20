5b105cffe8c2d9a4e8ba18a82fc2db5a313749c8
phai-zk <chisanupong.m34.1@gmail.com>
2025-06-20 22:44:52 +0700

    update

--- utils/special_test/quiz8_test.c
+++ utils/special_test/quiz8_test.c
@@ -1,32 +1,6 @@
 #include <stdio.h>
 #include "../utils.h"
 
-// void ft_get_minesweeper_board(int board[9][9])
-// {
-// 	int x;
-// 	int y;
-
-// 	srand(time(NULL));
-// 	for (int i = 0; i < 20; i++)
-// 	{
-// 		x = rand() % 9;
-// 		y = rand() % 9;
-// 		board[x][y] = 9;
-// 	}
-// }
-
-// void ft_print_minesweeper_board(int board[9][9])
-// {
-// 	for (int i = 0; i < 9; i++)
-// 	{
-// 		for (int j = 0; j < 9; j++)
-// 		{
-// 			printf(" %d ", board[i][j]);
-// 		}
-// 		printf("\n");
-// 	}
-// }
-
 int main()
 {
 	int board[9][9] =  {{0, 0, 0, 0, 0, 0, 0, 0, 0},
